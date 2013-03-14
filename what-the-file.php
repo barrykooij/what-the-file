552e65d2bcfdf5b2f0e079dbebf1794de42a2acb
Barry Kooij <git@barrykooij.nl>
2013-03-14 11:48:14 +0100

    Added GPL

--- what-the-file.php
+++ what-the-file.php
@@ -6,6 +6,23 @@
   Version: 1.1.2
   Author: Barry Kooij
   Author URI: http://www.barrykooij.com/
+	License: GPL v3
+
+	What The File Plugin
+	Copyright (C) 2012-2013, Barry Kooij - barry@cageworks.nl
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 class WhatTheFile
