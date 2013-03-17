be640a380ab0dfecc9d21e1fe76bf0d6b7bd0cbe
Barry Kooij <git@barrykooij.nl>
2013-03-17 16:54:11 +0100

    Changed admin and administrator role check

--- what-the-file.php
+++ what-the-file.php
@@ -29,17 +29,17 @@ class WhatTheFile
 {
 	const OPTION_INSTALL_DATE 			= 'whatthefile-install-date';
 	const OPTION_ADMIN_NOTICE_KEY 	= 'whatthefile-hide-notice';
-  private $template_name  				= '';
+	private $template_name  				= '';
 
 	public static function plugin_activation()
 	{
 		self::insert_install_date();
 	}
 
-  public function __construct()
-  {
+	public function __construct()
+	{
 		$this->hooks();
-  }
+	}
 
 	private static function insert_install_date()
 	{
@@ -59,13 +59,6 @@ class WhatTheFile
 		return new DateTime( $date_string );
 	}
 
-	private function check_admin()
-	{
-		if( !is_super_admin() || !is_admin_bar_showing() ){ return false; }
-		if( is_admin() ){ return false; }
-		return true;
-	}
-
 	private function get_current_page()
 	{
 		return $this->template_name;
@@ -79,11 +72,16 @@ class WhatTheFile
 
 	private function hooks()
 	{
+		// Check if user is an administrator
+		if( !current_user_can('manage_options') ) {
+			return false;
+		}
+
 		// Admin notice hide catch
 		add_action( 'admin_init', array( $this, 'catch_hide_notice' ) );
 
 		// Is admin notice hidden?
-		global $current_user;
+		$current_user = wp_get_current_user();
 		$hide_notice = get_user_meta( $current_user->ID, self::OPTION_ADMIN_NOTICE_KEY, true );
 
 		if( current_user_can( 'install_plugins' ) && $hide_notice == '' ) {
@@ -97,9 +95,9 @@ class WhatTheFile
 			}
 		}
 
-		// Check if is frontend and user is admin
-		if( !$this->check_admin() ) {
-			return false;
+		// Don't add admin bar option in admin panel
+		if( is_admin() || !is_admin_bar_showing() ) {
+			return;
 		}
 
 		// WTF actions and filers
@@ -146,8 +144,8 @@ class WhatTheFile
 		$query_string = '?' . http_build_query( array_merge( $query_params, array( self::OPTION_ADMIN_NOTICE_KEY => '1' ) ) );
 
 		echo '<div class="updated"><p>';
-    printf(__("You've been using <b>What The File</b> for some time now, could you please give it a review at wordpress.org? <br /><br /> <a href='%s' target='_blank'>Yes, take me there!</a> - <a href='%s'>I've already done this!</a>"), 'http://wordpress.org/support/view/plugin-reviews/what-the-file', $query_string );
-   	echo "</p></div>";
+		printf(__("You've been using <b>What The File</b> for some time now, could you please give it a review at wordpress.org? <br /><br /> <a href='%s' target='_blank'>Yes, take me there!</a> - <a href='%s'>I've already done this!</a>"), 'http://wordpress.org/support/view/plugin-reviews/what-the-file', $query_string );
+		echo "</p></div>";
 	}
 
 	public function save_buddy_press_template( $template )
@@ -156,9 +154,9 @@ class WhatTheFile
 			$this->template_name = str_ireplace( get_template_directory() . '/', '', $template );
 		}
 	}
-  
-  public function save_current_page( $template_name )
-  {
+
+	public function save_current_page( $template_name )
+	{
 		$this->template_name = basename( $template_name );
 
 		// Do Roots Theme check
@@ -166,20 +164,20 @@ class WhatTheFile
 			$this->template_name = basename( roots_template_path() );
 		}
 
-    return $template_name;
-  }
+		return $template_name;
+	}
 
-  public function admin_bar_menu() {
-    global $wp_admin_bar;      
-    $wp_admin_bar->add_menu( array( 'id' => 'wtf-bar'			, 'parent' => 'top-secondary'	, 'title' => __( 'What The File', 'what-the-file' )	, 'href' => FALSE ) );
-    $wp_admin_bar->add_menu( array( 'id' => 'wtf-bar-sub'	, 'parent' => 'wtf-bar'				, 'title' => $this->get_current_page()							, 'href' => get_admin_url() . 'theme-editor.php?file=' . $this->get_current_page() . '&theme=' . get_template() ) );
-  }
-  
-  public function print_css()
-  {
-    echo "<style type=\"text/css\" media=\"screen\">#wp-admin-bar-wtf-bar #wp-admin-bar-wtf-bar-sub .ab-item{display: block !important;text-align: right;}</style>\n";
-  }
-  
+	public function admin_bar_menu() {
+		global $wp_admin_bar;
+		$wp_admin_bar->add_menu( array( 'id' => 'wtf-bar'			, 'parent' => 'top-secondary'	, 'title' => __( 'What The File', 'what-the-file' )	, 'href' => FALSE ) );
+		$wp_admin_bar->add_menu( array( 'id' => 'wtf-bar-sub'	, 'parent' => 'wtf-bar'				, 'title' => $this->get_current_page()							, 'href' => get_admin_url() . 'theme-editor.php?file=' . $this->get_current_page() . '&theme=' . get_template() ) );
+	}
+
+	public function print_css()
+	{
+		echo "<style type=\"text/css\" media=\"screen\">#wp-admin-bar-wtf-bar #wp-admin-bar-wtf-bar-sub .ab-item{display: block !important;text-align: right;}</style>\n";
+	}
+
 }
 
 add_action( 'plugins_loaded', create_function( '', 'new WhatTheFile();' ) );
