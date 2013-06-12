12ca61306f34f1fd9243dea3c4a75c4b705a891e
Barry Kooij <git@barrykooij.nl>
2013-06-12 14:56:01 +0200

    Updated meta information

--- what-the-file.php
+++ what-the-file.php
@@ -1,19 +1,22 @@
 <?php
 /*
   Plugin Name: What The File
+	Contributors: barrykooij
   Plugin URI: http://www.barrykooij.com/what-the-file/
+	Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q7QDZTLCRKSMG
   Description: What The File adds an option to your toolbar showing what file and template parts are used to display the page you’re currently viewing. You can click the file name to directly edit it through the theme editor. Supports BuddyPress and Roots Theme. More information can be found at the <a href='http://wordpress.org/extend/plugins/what-the-file/'>WordPress plugin page</a>.
   Version: 1.3.2
   Author: Barry Kooij
   Author URI: http://www.barrykooij.com/
-	License: GPL v3
+	License: GPLv2 or later
+	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 	What The File Plugin
 	Copyright (C) 2012-2013, Barry Kooij - barry@cageworks.nl
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
+	the Free Software Foundation, either version 2 of the License, or
 	(at your option) any later version.
 
 	This program is distributed in the hope that it will be useful,
