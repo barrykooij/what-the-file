67d90713fe107a446abf8edcc13a0ce044d1678d
Barry Kooij <git@barrykooij.nl>
2015-08-18 14:03:12 +0200

    added 'powered by' top admin ba

--- what-the-file.php
+++ what-the-file.php
@@ -227,7 +227,7 @@ class WhatTheFile {
 		$wp_admin_bar->add_menu( array(
 			'id'     => 'wtf-bar',
 			'parent' => 'top-secondary',
-			'title'  => __( 'What The File', 'what-the-file' ),
+			'title'  => __( 'What The File', 'what-the-file' ) . '<img src="'.plugins_url( 'assets/images/never5-logo.png', __FILE__ ).'" />',
 			'href'   => false
 		) );
 
@@ -275,13 +275,22 @@ class WhatTheFile {
 			}
 
 		}
+
+		// Add powered by
+		$wp_admin_bar->add_menu( array(
+			'id'     => 'wtf-bar-powered-by',
+			'parent' => 'wtf-bar',
+			'title'  => 'Powered by Never5',
+			'href'   => 'http://www.never5.com'
+		) );
+
 	}
 
 	/**
 	 * Print the custom CSS
 	 */
 	public function print_css() {
-		echo "<style type=\"text/css\" media=\"screen\">#wp-admin-bar-wtf-bar #wp-admin-bar-wtf-bar-template-file .ab-item, #wp-admin-bar-wtf-bar #wp-admin-bar-wtf-bar-template-parts {text-align:right;} #wp-admin-bar-wtf-bar-template-parts.menupop > .ab-item:before{ right:auto !important; }</style>\n";
+		echo "<style type=\"text/css\" media=\"screen\">#wp-admin-bar-wtf-bar img {margin:-4px 0 0 6px;}#wp-admin-bar-wtf-bar #wp-admin-bar-wtf-bar-template-file .ab-item, #wp-admin-bar-wtf-bar #wp-admin-bar-wtf-bar-template-parts {text-align:right;} #wp-admin-bar-wtf-bar-template-parts.menupop > .ab-item:before{ right:auto !important; }#wp-admin-bar-wtf-bar-powered-by{text-align: right;}#wp-admin-bar-wtf-bar-powered-by a{color:#ffa100 !important;}</style>\n";
 	}
 
 }
